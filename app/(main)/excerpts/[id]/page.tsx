47737cb96faa375e0f3d1b72830216faa7a6f365
Myles Moylan <moylanm@fastmail.com>
2024-10-29 12:38:25 -0700

    Stub out excerpt detail page

--- app/(main)/excerpts/[id]/page.tsx
+++ app/(main)/excerpts/[id]/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-  return <h2>Excerpt detail page...</h2>;
+import { fetchExcerptById, fetchExcerpts } from "@/app/lib/data";
+import type { Excerpt } from "@/app/lib/definitions";
+import Markdown from 'react-markdown';
+
+export async function generateStaticParams() {
+  return await fetchExcerpts();
+}
+
+export default async function Page({
+  params
+}: {
+  params: Excerpt
+}) {
+  const { id } = await params;
+  const {
+    author,
+    work,
+    body
+  } = await fetchExcerptById(id);
+
+  return (
+    <>
+      <h2>Excerpt detail page...</h2>
+      <h3>{author}</h3>
+      <h3>{work}</h3>
+      <Markdown>{body}</Markdown>
+    </>
+  );
 }
