3b7c0d04589d51100417b9956362630e3184b580
Myles Moylan <moylanm@fastmail.com>
2024-10-29 14:30:49 -0700

    Add style file for mui components

---
+++ app/ui/style.ts
@@ -0,0 +1,23 @@
+'use client'
+
+import { styled } from '@mui/system';
+import type { TableContainerProps } from '@mui/material';
+import { TableContainer } from '@mui/material';
+import Link, { type LinkProps } from 'next/link';
+
+export const StyledTableContainer = styled(TableContainer)<TableContainerProps>({
+	marginLeft: 'auto',
+	marginRight: 'auto',
+	maxWidth: '800px',
+	overflow: 'auto',
+	alignItems: 'center',
+	justifyContent: 'center'
+});
+
+export const StyledLink = styled(Link)<LinkProps>({
+	'&:hover': {
+		textDecoration: 'underline'
+	},
+	textDecoration: 'none',
+	color: '#62CB31'
+});
