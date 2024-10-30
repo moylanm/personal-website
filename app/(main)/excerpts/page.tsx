322638d4367e198e53be04f1e433f99aa144891d
Myles Moylan <moylanm@fastmail.com>
2024-10-30 06:28:50 -0700

    Cache full excerpt list

--- app/(main)/excerpts/page.tsx
+++ app/(main)/excerpts/page.tsx
@@ -1,9 +1,18 @@
 import { fetchAllExcerpts } from "@/app/_lib/data";
 import FilterForm from "./_components/FilterForm";
 import List from "./_components/List";
+import { unstable_cache } from "next/cache";
+
+const getAllExcerpts = unstable_cache(
+  async () => {
+    return await fetchAllExcerpts();
+  },
+  ['excerpts'],
+  { revalidate: 3600, tags: ['excerpts'] }
+);
 
 export default async function Page() {
-  const excerpts = await fetchAllExcerpts();
+  const excerpts = await getAllExcerpts();
 
   return (
     <>
