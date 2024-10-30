aa491ad18ac6e8c2ed0ab72cf0986d560b447dac
Myles Moylan <moylanm@fastmail.com>
2024-10-30 09:56:31 -0700

    Add Vercel analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { merriweather } from './_ui/fonts';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 import theme from './_ui/theme';
 import Navbar from './_ui/_components/Navbar';
-import { CssBaseline } from '@mui/material';
 import Footer from './_ui/_components/Footer';
 
 export const metadata: Metadata = {
@@ -29,6 +31,8 @@ export default function RootLayout({
             <Footer />
           </ThemeProvider>
         </AppRouterCacheProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
