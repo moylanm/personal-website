b5c7efd578b2ad294583d2c8199d28467c72bbcd
Myles Moylan <moylanm@fastmail.com>
2024-10-30 09:39:29 -0700

    Add about page and associated styling

--- app/_ui/style.ts
+++ app/_ui/style.ts
@@ -1,8 +1,8 @@
 'use client'
 
-import { styled } from '@mui/system';
+import { Container, styled } from '@mui/system';
 import type { TableContainerProps } from '@mui/material';
-import { TableContainer } from '@mui/material';
+import { Card, Grid2, TableContainer } from '@mui/material';
 import Link, { type LinkProps } from 'next/link';
 
 export const StyledTableContainer = styled(TableContainer)<TableContainerProps>({
@@ -20,3 +20,20 @@ export const StyledLink = styled(Link)<LinkProps>({
 	textDecoration: 'none',
 	color: '#62CB31'
 });
+
+export const StyledContainer = styled(Container)({
+	marginTop: '150px',
+	marginBottom: '150px',
+	marginLeft: 'auto',
+	marginRight: 'auto',
+	maxWidth: '800px'
+});
+
+export const StyledGrid2 = styled(Grid2)({
+	alignItems: 'center',
+	justifyContent: 'center',
+});
+
+export const StyledCard = styled(Card)({
+	padding: '30px'
+})
