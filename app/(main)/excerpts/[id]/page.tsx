a9b0ac005aab99ecedfe9328553850b0b8a2b552
Myles Moylan <moylanm@fastmail.com>
2024-10-31 16:46:04 -0700

    Adjust method of retrieving id

--- app/(main)/excerpts/[id]/page.tsx
+++ app/(main)/excerpts/[id]/page.tsx
@@ -18,7 +18,7 @@ export async function generateMetadata({
 }: {
 	params: Promise<{ id: string }>
 }): Promise<Metadata> {
-	const { id } = await params;
+	const id = (await params).id;
 
 	const { author } = await getExcerpt(id);
 
@@ -32,7 +32,7 @@ export default async function Page({
 }: {
   params: Promise<{ id: string }>
 }) {
-  const { id } = await params;
+  const id = (await params).id;
   const {
     author,
     work,
