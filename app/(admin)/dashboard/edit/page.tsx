fe70a8a7fd01c57ec75df22c063039fb009e723d
Myles Moylan <moylanm@fastmail.com>
2024-11-03 19:13:33 -0800

    Stub out publish and edit pages

---
+++ app/(admin)/dashboard/edit/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Excerpt'
+};
+
+export default async function Page() {
+  return (
+    <>
+      Edit page...
+    </>
+  );
+}
---
+++ app/(admin)/dashboard/publish/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Publish Excerpt'
+};
+
+export default async function Page() {
+  return (
+    <>
+      Publish page...
+    </>
+  );
+}
