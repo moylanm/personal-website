fc2b2b2eee3e55aee8d63584573b81fbadba6fa4
Myles Moylan <moylanm@fastmail.com>
2024-11-04 10:10:18 -0800

    Add StoreProvider for dashboard

--- app/(admin)/dashboard/layout.tsx
+++ app/(admin)/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
+import StoreProvider from './StoreProvider';
 import SideNav from '@/app/ui/dashboard/SideNav';
 import {
   DashboardLayoutBox,
@@ -15,20 +16,22 @@ export default async function Layout({ children }: { children: React.ReactNode }
 
   return (
     <>
-      <Grid2 container>
-        <Grid2 size={2}>
-          <DashboardLayoutBox>
-            <DashboardLayoutSideNavBox>
-              <SideNav />
-            </DashboardLayoutSideNavBox>
-          </DashboardLayoutBox>
+      <StoreProvider>
+        <Grid2 container>
+          <Grid2 size={2}>
+            <DashboardLayoutBox>
+              <DashboardLayoutSideNavBox>
+                <SideNav />
+              </DashboardLayoutSideNavBox>
+            </DashboardLayoutBox>
+          </Grid2>
+          <Grid2>
+            <DashboardLayoutChildrenBox>
+              {children}
+            </DashboardLayoutChildrenBox>
+          </Grid2>
         </Grid2>
-        <Grid2>
-          <DashboardLayoutChildrenBox>
-            {children}
-          </DashboardLayoutChildrenBox>
-        </Grid2>
-      </Grid2>
+      </StoreProvider>
     </>
   );
 }
