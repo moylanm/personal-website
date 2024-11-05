415148193c269227fd80449ea3cb8e171f7b7a9f
Myles Moylan <moylanm@fastmail.com>
2024-11-05 15:10:57 -0800

    Add helper functions for db actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
+import { revalidateTag } from 'next/cache';
+import { deleteExcerptFromDb, publishExcerptToDb, updateExcerptInDb } from './data';
 
 export async function authenticate(
   prevState: string | undefined,
@@ -24,4 +26,55 @@ export async function authenticate(
     }
     throw error;
   }
+}
+
+export async function publishExcerpt({
+  author,
+  work,
+  body
+}: {
+  author: string,
+  work: string,
+  body: string
+}) {
+  await publishExcerptToDb({
+    author,
+    work,
+    body
+  });
+  revalidateExcerptTags();
+}
+
+export async function updateExcerpt({
+  id,
+  author,
+  work,
+  body
+}: {
+  id: string,
+  author: string,
+  work: string,
+  body: string
+}) {
+  await updateExcerptInDb({
+    id,
+    author,
+    work,
+    body
+  });
+  revalidateExcerptTags();
+}
+
+export async function deleteExcerpt({
+  id
+}: {
+  id: string
+}) {
+  await deleteExcerptFromDb({ id: id });
+  revalidateExcerptTags();
+}
+
+function revalidateExcerptTags() {
+  revalidateTag('latest');
+  revalidateTag('excerpts');
 }
