ef2be81da639903e41c7e2654465f2e1e1be9dae
Myles Moylan <moylanm@fastmail.com>
2024-11-05 11:25:53 -0800

    Fix bug preventing store from loading

--- app/(admin)/dashboard/StoreProvider.tsx
+++ app/(admin)/dashboard/StoreProvider.tsx
@@ -3,16 +3,21 @@
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore, type AppStore } from '@/lib/dashboard/store'
+import type { Excerpt } from '@/app/lib/definitions';
+import { initializeState } from '@/lib/dashboard/features/excerpts/excerptsSlice';
 
 export default function StoreProvider({
+  excerpts,
   children
 }: {
+  excerpts: Excerpt[]
   children: React.ReactNode
 }) {
-  const storeRef = useRef<AppStore>()
+  const storeRef = useRef<AppStore | null>(null)
 
   if (!storeRef.current) {
     storeRef.current = makeStore()
+    storeRef.current.dispatch(initializeState(excerpts));
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>
