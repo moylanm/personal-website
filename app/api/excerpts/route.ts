6629cdd8f5d6160fb10c87758698e846cae31c5a
Myles Moylan <moylanm@fastmail.com>
2024-11-06 03:41:35 -0800

    Add excerpt handling API

---
+++ app/api/excerpts/route.ts
@@ -0,0 +1,109 @@
+import { NextResponse } from 'next/server';
+import { auth } from '@/auth';
+import {
+	allExcerpts,
+	latestExcerpts,
+	excerptById,
+	publishExcerptToDb,
+	updateExcerptInDb,
+	deleteExcerptFromDb
+} from '@/app/lib/data';
+
+async function checkAuth() {
+	const session = await auth();
+
+	if (!session) {
+		return NextResponse.json(
+			{ error: 'Unauthorized' },
+			{ status: 401 }
+		);
+	}
+
+	return session;
+}
+
+export async function GET(request: Request) {
+	const { searchParams } = new URL(request.url);
+	const id = searchParams.get('id');
+	const count = searchParams.get('count');
+
+	try {
+		if (id) {
+			const data = await excerptById(id);
+			return NextResponse.json(data);
+		}
+
+		if (count) {
+			const data = await latestExcerpts(Number(count));
+			return NextResponse.json(data);
+		}
+
+		const data = await allExcerpts();
+		return NextResponse.json(data);
+	} catch {
+		return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+	}
+}
+
+export async function POST(request: Request) {
+	const session = await checkAuth();
+	if (session instanceof NextResponse) return session;
+
+	try {
+		const {
+			author,
+			work,
+			body
+		} = await request.json();
+
+		await publishExcerptToDb({
+			author,
+			work,
+			body
+		});
+
+		return NextResponse.json({ success: true });
+	} catch {
+		return NextResponse.json({ error: 'Internal Server Error '}, { status: 500 });
+	}
+}
+
+export async function PUT(request: Request) {
+	const session = await checkAuth();
+	if (session instanceof NextResponse) return session;
+
+	try {
+		const {
+			id,
+			author,
+			work,
+			body
+		} = await request.json();
+		
+		await updateExcerptInDb({
+			id,
+			author,
+			work,
+			body
+		});
+		
+		return NextResponse.json({ success: true });
+	} catch {
+		return NextResponse.json({ error: 'Internal Server Error '}, { status: 500 });
+	}
+}
+
+export async function DELETE(request: Request) {
+	const session = await checkAuth();
+	if (session instanceof NextResponse) return session;
+
+	try {
+		const { id } = await request.json();
+
+		await deleteExcerptFromDb({ id });
+		
+		return NextResponse.json({ success: true });
+	} catch {
+		return NextResponse.json({ error: 'Internal Server Error '}, { status: 500 });
+	}
+}
