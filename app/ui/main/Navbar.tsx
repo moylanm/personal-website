214b14e865cf62b19cc8fb30a985a83a447839c8
Myles Moylan <moylanm@fastmail.com>
2024-11-09 13:07:20 -0800

    Add back the Typography component which keeps the logo centered

--- app/ui/main/Navbar.tsx
+++ app/ui/main/Navbar.tsx
@@ -79,6 +79,16 @@ const Navbar = () => {
             </Menu>
           </Box>
           <NavbarLogo />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+            }}
+          />
           <Box sx={{ marginLeft: 2, flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {PAGES.map((page) => (
                <Button
