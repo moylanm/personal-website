be82bfbad0f509ada46b41c55985b641196618ec
Myles Moylan <moylanm@fastmail.com>
2024-11-08 17:29:21 -0800

    Use single quotes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   logging: {
     fetches: {
       fullUrl: true,
     }
-  }
+  },
 };
 
 export default nextConfig;
