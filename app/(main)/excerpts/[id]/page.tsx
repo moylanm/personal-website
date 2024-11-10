e28a4b7bf3d76b67bfda8f84deda7360c9213acb
Myles Moylan <moylanm@fastmail.com>
2024-11-10 13:24:37 -0800

    Test excerpt body skeleton

--- app/(main)/excerpts/[id]/page.tsx
+++ app/(main)/excerpts/[id]/page.tsx
@@ -1,20 +1,42 @@
 import type { Metadata } from 'next';
 import { excerptById } from '@/lib/data';
-import { Card, CardContent, Container, Typography } from '@mui/material';
+import { Card, CardContent, Container, Typography, Skeleton } from '@mui/material';
 import Markdown from 'react-markdown';
+import { Suspense } from 'react';
+
+async function ExcerptBody({ id }: { id: string }) {
+  const { body } = await excerptById(id);
+
+  return (
+    <Markdown>
+      {body}
+    </Markdown>
+  );
+}
+
+function BodySkeleton() {
+  return (
+    <div style={{ marginTop: '1rem' }}>
+      <Skeleton height={20} />
+      <Skeleton height={20} />
+      <Skeleton height={20} width="80%" />
+      <Skeleton height={20} width="90%" />
+      <Skeleton height={20} width="75%" />
+    </div>
+  );
+}
 
 export async function generateMetadata({
-	params
+  params
 }: {
-	params: Promise<{ id: string }>
+  params: Promise<{ id: string }>
 }): Promise<Metadata> {
-	const id = (await params).id;
+  const id = (await params).id;
+  const excerpt = await excerptById(id);
 
-	const { author } = await excerptById(id);
-
-	return {
-		title: author
-	};
+  return {
+    title: excerpt.author
+  };
 }
 
 export default async function Page({
@@ -23,33 +45,30 @@ export default async function Page({
   params: Promise<{ id: string }>
 }) {
   const id = (await params).id;
-  const {
-    author,
-    work,
-    body
-  } = await excerptById(id);
+  const { author, work } = await excerptById(id);
 
   return (
-		<Container maxWidth='md'>
-			<Card sx={{
-				p: 2,
-				mt: '180px',
-				mb: '110px',
-				mx: 3,
-				display: 'flex',
-				justifyContent: 'center',
-			}}>
-				<CardContent>
-					<Typography sx={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
-						{author}
-						<br />
-						{work}
-					</Typography>
-					<Markdown>
-						{body}
-					</Markdown>
-				</CardContent>
-			</Card>
-		</Container>
-	);
+    <Container maxWidth='md'>
+      <Card sx={{
+        p: 2,
+        mt: '180px',
+        mb: '110px',
+        mx: 3,
+        display: 'flex',
+        justifyContent: 'center',
+      }}>
+        <CardContent>
+          <Typography sx={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
+            {author}
+            <br />
+            {work}
+          </Typography>
+
+          <Suspense fallback={<BodySkeleton />}>
+            <ExcerptBody id={id} />
+          </Suspense>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 }
