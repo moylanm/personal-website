aeb20a17cfc607e7f21e021c5b7cfca4edb5d59a
Myles Moylan <moylanm@fastmail.com>
2024-11-10 13:49:02 -0800

    Adjust skeleton

--- app/ui/excerpts/BodySkeleton.tsx
+++ app/ui/excerpts/BodySkeleton.tsx
@@ -3,7 +3,11 @@ import { Box, Skeleton } from '@mui/material';
 export default function BodySkeleton() {
   return (
     <Box sx={{ mt: 1 }}>
-      <Skeleton width={740} height={200} />
+      <Skeleton width={740} height={20} />
+      <Skeleton width={740} height={20} /> 
+      <Skeleton width='80%' height={20} />
+      <Skeleton width='90%' height={20} />
+      <Skeleton width='75%' height={20} />
     </Box>
   );
 }
