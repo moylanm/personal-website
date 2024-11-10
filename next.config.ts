40ac727dcfd6d41a6a99519427c53c042a10754d
Myles Moylan <moylanm@fastmail.com>
2024-11-10 07:55:58 -0800

    Adjust HTTP headers

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,28 @@ const nextConfig: NextConfig = {
       fullUrl: true,
     }
   },
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://mylesmoylan.net'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      }
+    ];
+  },
 };
 
 export default nextConfig;
