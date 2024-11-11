14e1f3a7beaa77eee9b26a27760fa692620a4926
Myles Moylan <moylanm@fastmail.com>
2024-11-10 16:48:37 -0800

    Make skeleton components wavy

--- app/ui/excerpts/ExcerptSkeleton.tsx
+++ app/ui/excerpts/ExcerptSkeleton.tsx
@@ -8,14 +8,14 @@ export default function ExcerptSkeleton() {
     <>
       <SkeletonContainer>
         <Box sx={{ textAlign: 'center', marginBottom: '1rem' }}>
-          <Skeleton width="60%" height={28} style={{ margin: '0 auto' }} />
-          <Skeleton width="40%" height={28} style={{ margin: '0 auto' }} />
+          <Skeleton animation='wave' width='60%' height={28} style={{ margin: '0 auto' }} />
+          <Skeleton animation='wave' width="40%" height={28} style={{ margin: '0 auto' }} />
         </Box>
-        <Skeleton height={20} />
-        <Skeleton height={20} />
-        <Skeleton height={20} width="80%" />
-        <Skeleton height={20} width="90%" />
-        <Skeleton height={20} width="75%" />
+        <Skeleton animation='wave' height={20} />
+        <Skeleton animation='wave' height={20} />
+        <Skeleton animation='wave' height={20} width="80%" />
+        <Skeleton animation='wave' height={20} width="90%" />
+        <Skeleton animation='wave' height={20} width="75%" />
       </SkeletonContainer>
     </>
   );
