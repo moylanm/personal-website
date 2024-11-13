db8163349eb4fa1a532b5b80a5cf54d56f72d68d
Myles Moylan <moylanm@fastmail.com>
2024-11-13 06:52:55 -0800

    Add missing semicolon

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
  
 export function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
