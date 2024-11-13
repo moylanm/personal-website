d85340fd8ff5b5d690e58270f61d29737cdc977c
Myles Moylan <moylanm@fastmail.com>
2024-11-13 06:50:08 -0800

    Use next-auth/react signIn function

--- app/ui/login/LoginForm.tsx
+++ app/ui/login/LoginForm.tsx
@@ -1,13 +1,32 @@
 'use client';
 
-import { authenticate } from '@/lib/actions';
 import { useActionState } from 'react';
 import { Box, Button, TextField } from '@mui/material';
 import { LoginFormContainer, LoginFormCard } from '../style';
+import { signIn } from 'next-auth/react';
+import { AuthError } from 'next-auth';
 
 export default function LoginForm() {
   const [errorMessage, formAction, isPending] = useActionState(
-    authenticate,
+    async (_: unknown, formData: FormData) => {
+      try {
+        await signIn('credentials', {
+          email: formData.get('email'),
+          password: formData.get('password'),
+          redirectTo: '/dashboard',
+        });
+      } catch (error) {
+        if (error instanceof AuthError) {
+          switch (error.type) {
+            case 'CredentialsSignin':
+              return 'Invalid credentials.';
+            default:
+              return 'Something went wrong.';
+          }
+        }
+        throw error;
+      }
+    },
     undefined
   );
 
