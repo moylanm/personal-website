82aff5f6346a13215c1721bfd991716d1a09ec24
Myles Moylan <moylanm@fastmail.com>
2024-11-15 09:22:23 -0800

    Metadata improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     template: '%s | mylesmoylan.net',
     default: 'mylesmoylan.net',
   },
-  description: 'The personal website of Myles Moylan',
+  description: 'The personal website of Myles Moylan - Exploring philosophy, psychology, religion, and literature',
   creator: 'Myles Moylan',
   keywords: [
     'philosophy',
@@ -27,6 +27,32 @@ export const metadata: Metadata = {
     'Buddhism',
     'literature',
   ],
+  metadataBase: new URL('https://mylesmoylan.net'),
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://mylesmoylan.net',
+    siteName: 'mylesmoylan.net',
+    title: 'mylesmoylan.net',
+    description: 'The personal website of Myles Moylan - Exploring philosophy, psychology, religion, and literature',
+    images: [
+      {
+        url: '/og-image-home.png',
+        width: 1200,
+        height: 630,
+        alt: 'mylesmoylan.net - Personal Website'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'mylesmoylan.net',
+    description: 'The personal website of Myles Moylan - Exploring philosophy, psychology, religion, and literature',
+    images: ['/og-image-home.png'],
+  },
+  alternates: {
+    canonical: 'https://mylesmoylan.net'
+  }
 };
 
 export default function RootLayout({
