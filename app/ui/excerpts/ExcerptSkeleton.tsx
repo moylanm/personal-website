eb6946fab375be8b9f6e1bd06619f4ec1bac6a0a
Myles Moylan <moylanm@fastmail.com>
2024-11-15 07:16:15 -0800

    Adjust styling

--- app/ui/excerpts/ExcerptSkeleton.tsx
+++ app/ui/excerpts/ExcerptSkeleton.tsx
@@ -1,21 +1,23 @@
 'use client'
 
-import { Box, Skeleton } from "@mui/material";
+import { Box, Paper, Skeleton } from "@mui/material";
 import { SkeletonContainer } from "../style";
 
 export default function ExcerptSkeleton() {
   return (
     <>
       <SkeletonContainer>
-        <Box sx={{ textAlign: 'center', marginBottom: '1rem' }}>
-          <Skeleton animation='wave' width='60%' height={28} style={{ margin: '0 auto' }} />
-          <Skeleton animation='wave' width="40%" height={28} style={{ margin: '0 auto' }} />
-        </Box>
-        <Skeleton animation='wave' height={20} />
-        <Skeleton animation='wave' height={20} />
-        <Skeleton animation='wave' height={20} width="80%" />
-        <Skeleton animation='wave' height={20} width="90%" />
-        <Skeleton animation='wave' height={20} width="75%" />
+        <Paper elevation={2} sx={{ p: { xs: 2, sm: 4 }, borderRadius: 2 }}>
+          <Box sx={{ textAlign: 'center', marginBottom: '1rem' }}>
+            <Skeleton animation='wave' width='60%' height={28} style={{ margin: '0 auto' }} />
+            <Skeleton animation='wave' width="40%" height={28} style={{ margin: '0 auto' }} />
+          </Box>
+          <Skeleton animation='wave' height={20} />
+          <Skeleton animation='wave' height={20} />
+          <Skeleton animation='wave' height={20} width="80%" />
+          <Skeleton animation='wave' height={20} width="90%" />
+          <Skeleton animation='wave' height={20} width="75%" />
+        </Paper>
       </SkeletonContainer>
     </>
   );
