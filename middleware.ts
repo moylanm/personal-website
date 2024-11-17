f0c7f1576fad243795b4816b7769ad1183a37007
Myles Moylan <moylanm@fastmail.com>
2024-11-17 08:02:33 -0800

    CSP fix

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,8 @@ export function middleware(request: NextRequest) {
 
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' ${process.env.NODE_ENV === "production" ? "" : "'unsafe-eval'"};
+    script-src 'self' 'nonce-${nonce}' ${process.env.NODE_ENV === "production" ? "'strict-dynamic'" : "'unsafe-eval'"};
+    script-src-elem 'self' 'nonce-${nonce}';
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
