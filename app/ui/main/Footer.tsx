c4c5038fa1313446dbbe2b9196f9328817bbf85c
Myles Moylan <moylanm@fastmail.com>
2024-11-16 16:02:03 -0800

    Use single quotes

--- app/ui/main/Footer.tsx
+++ app/ui/main/Footer.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-import { Box } from "@mui/material";
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import { Box } from '@mui/material';
 
 const Footer = () => {
   return (
@@ -11,19 +11,19 @@ const Footer = () => {
       sx={{
         py: 3,
         bottom: 0,
-        width: "100%",
-        position: "fixed",
-        backgroundColor: "#282828"
+        width: '100%',
+        position: 'fixed',
+        backgroundColor: '#282828'
       }}
     >
       <Container>
-        <Typography variant="body2" color="text.secondary" align="center">
-            {"Powered by "}
-          <Link color="inherit" href="https://nextjs.org/">
+        <Typography variant='body2' color='text.secondary' align='center'>
+            {'Powered by '}
+          <Link color='inherit' href='https://nextjs.org/'>
             Next.js
-          </Link>{" in "}
+          </Link>{' in '}
           {new Date().getFullYear()}
-          {"."}
+          {'.'}
         </Typography>
       </Container>
     </Box>
