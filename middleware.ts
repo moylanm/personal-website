a78209bda902c4f49d5b177e72a1053fd3a1f027
Myles Moylan <moylanm@fastmail.com>
2024-11-17 06:53:34 -0800

    Whitelist over blacklist

--- middleware.ts
+++ middleware.ts
@@ -45,12 +45,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico|.*\\.png|.*\\.php|excerpts$|about|login|$).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
+    '/dashboard/:path*',
   ],
 };
