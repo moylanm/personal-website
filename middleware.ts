4c3affc679dfecb586aa43cb5558fccd79a45322
Myles Moylan <moylanm@fastmail.com>
2024-11-17 18:14:39 -0800

    Simplify CSP

--- middleware.ts
+++ middleware.ts
@@ -6,15 +6,11 @@ export function middleware(request: NextRequest) {
   const cspHeader = [
     "default-src 'self'",
     "style-src 'self' 'unsafe-inline'",
+    `script-src 'self' 'unsafe-inline' ${isProd ? "" : "'unsafe-eval'"}`,
     "img-src 'self' blob: data:",
-    `script-src 'self' ${isProd ? "" : "'unsafe-eval'"} 'unsafe-inline'`,
-    "font-src 'self'",
     "object-src 'none'",
-    "base-uri 'self'",
-    "form-action 'self'",
     "frame-ancestors 'none'",
-    "connect-src 'self'",
-    "worker-src 'self'",
+    "form-action 'self'",
     "upgrade-insecure-requests",
   ].join('; ');
 
