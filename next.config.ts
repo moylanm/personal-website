de72d9997a98c72f2ee1f6580ecd61a5028fb0dc
Myles Moylan <moylanm@fastmail.com>
2024-11-17 18:31:06 -0800

    Move away from middleware for implementing CSP

--- next.config.ts
+++ next.config.ts
@@ -9,10 +9,25 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   async headers() {
+    const isProd = process.env.NODE_ENV === 'production';
+
     return [
       {
         source: '/:path*',
         headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "style-src 'self' 'unsafe-inline'",
+              `script-src 'self' 'unsafe-inline' ${isProd ? "" : "'unsafe-eval'"}`,
+              "img-src 'self' blob: data:",
+              "object-src 'none'",
+              "frame-ancestors 'none'",
+              "form-action 'self'",
+              "upgrade-insecure-requests",
+            ].join('; ')
+          },
           {
             key: 'Access-Control-Allow-Origin',
             value: 'https://mylesmoylan.net'
