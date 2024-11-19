03743a5e24c7ab0e465c8d7e7d7022fbe65d9687
Myles Moylan <moylanm@fastmail.com>
2024-11-19 04:42:14 -0800

    Adjust home table styles

--- styles/pages/home/home.styles.ts
+++ styles/pages/home/home.styles.ts
@@ -4,12 +4,14 @@ import { styled } from '@mui/system';
 import { TableContainer, type TableContainerProps } from '@mui/material';
 
 export const HomeTableContainer = styled(TableContainer)<TableContainerProps>(({theme}) => ({
-	marginTop: '180px',
-	marginBottom: '180px',
+	marginTop: '100px',
+	marginBottom: '100px',
 	marginLeft: 'auto',
 	marginRight: 'auto',
 	maxWidth: '400px',
 	[theme.breakpoints.up('sm')]: {
+		marginTop: '180px',
+		marginBottom: '180px',
 		maxWidth: '800px',
 	}
 }));
