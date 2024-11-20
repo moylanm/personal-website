e04f711f0816909ed394b8f9b70ba5eb89dd9c3b
Myles Moylan <moylanm@fastmail.com>
2024-11-20 07:58:21 -0800

    Create debounced search handler

---
+++ lib/useDebounceSearch.ts
@@ -0,0 +1,31 @@
+import { useState, useMemo, useEffect } from 'react';
+import { debounce } from 'lodash';
+
+const useDebounceSearch = (delay: number = 300) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [debouncedTerm, setDebouncedTerm] = useState('');
+
+  const debouncedCallback = useMemo(
+    () => debounce((term: string) => {
+      setDebouncedTerm(term);
+    }, delay),
+    [delay]
+  );
+
+  useEffect(() => {
+    debouncedCallback(searchTerm);
+
+    // Cleanup
+    return () => {
+      debouncedCallback.cancel();
+    };
+  }, [searchTerm, debouncedCallback]);
+
+  return {
+    searchTerm,
+    debouncedTerm,
+    setSearchTerm,
+  };
+};
+
+export default useDebounceSearch;
