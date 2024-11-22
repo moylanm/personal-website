7577167f4f447dc85e0387e394c556829bd0b761
Myles Moylan <moylanm@fastmail.com>
2024-11-22 05:17:45 -0800

    Remove unnecessary fragments

--- app/page.tsx
+++ app/page.tsx
@@ -35,38 +35,34 @@ export default async function Home() {
   const excerpts = await latestExcerpts(LATEST_COUNT);
 
   return (
-    <>
-      <HomeTableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Author & Work</TableCell>
-              <TableCell align='right'>Created</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {excerpts.map((excerpt) => <Row key={excerpt.id} excerpt={excerpt} />)}
-          </TableBody>
-        </Table>
-      </HomeTableContainer>
-    </>
+    <HomeTableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Author & Work</TableCell>
+            <TableCell align='right'>Created</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {excerpts.map((excerpt) => <Row key={excerpt.id} excerpt={excerpt} />)}
+        </TableBody>
+      </Table>
+    </HomeTableContainer>
   );
 }
 
 const Row: React.FC<{ excerpt: Excerpt }> = ({ excerpt }) => {
   return (
-    <>
-      <TableRow
-        key={excerpt.id}
-        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-      >
-        <TableCell component='th' scope='row'>
-          <ExcerptLink href={`excerpts/${excerpt.id}`}>
-            {`${excerpt.author} - ${excerpt.work}`}
-          </ExcerptLink>
-        </TableCell>
-        <TableCell align='right'>{new Date(excerpt.createdAt).toDateString()}</TableCell>
-      </TableRow>
-    </>
+    <TableRow
+      key={excerpt.id}
+      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    >
+      <TableCell component='th' scope='row'>
+        <ExcerptLink href={`excerpts/${excerpt.id}`}>
+          {`${excerpt.author} - ${excerpt.work}`}
+        </ExcerptLink>
+      </TableCell>
+      <TableCell align='right'>{new Date(excerpt.createdAt).toDateString()}</TableCell>
+    </TableRow>
   )
 };
