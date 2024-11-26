d2e62bd721727bcde34b36b14d842b13ea182876
Myles Moylan <moylanm@fastmail.com>
2024-11-26 09:26:07 -0800

    Use single quotes

--- app/excerpts/[id]/not-found.tsx
+++ app/excerpts/[id]/not-found.tsx
@@ -4,19 +4,19 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth='sm'>
       <NotFoundBox sx={{ mt: 1, gap: 2 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
+        <Typography variant='h4' component='h1' gutterBottom>
           Excerpt Not Found
         </Typography>
-        <Typography variant="body1" color="text.secondary">
+        <Typography variant='body1' color='text.secondary'>
           The excerpt you&apos;re looking for doesn&apos;t exist or has been removed.
         </Typography>
         <NotFoundButton
           sx={{ mt: 1 }}
           component={Link}
-          href="/excerpts" 
-          variant="outlined" 
+          href='/excerpts' 
+          variant='outlined' 
         >
           Back to Excerpts
         </NotFoundButton>
