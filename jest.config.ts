ab19fd671f945b7004b92919d9ac049a98a452f6
Myles Moylan <moylanm@fastmail.com>
2024-11-27 10:21:51 -0800

    Comment out `transformIgnorePatterns`

--- jest.config.ts
+++ jest.config.ts
@@ -189,9 +189,9 @@ const config: Config = {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: [
-    "/node_modules/(?!(@mui|@emotion|@hookform)/)",
-  ],
+  // transformIgnorePatterns: [
+  //   "/node_modules/(?!(@mui|@emotion|@hookform)/)",
+  // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
