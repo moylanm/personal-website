cecac04dce5ac07b69aa22f5e23bd101baa91958
Myles Moylan <moylanm@fastmail.com>
2024-11-28 11:33:22 -0800

    Add `Image`

--- __mocks__/next-mocks.tsx
+++ __mocks__/next-mocks.tsx
@@ -1,2 +1,8 @@
+import type { ImageProps } from 'next/image';
+
 export const Link = ({ children, href, ...props }: React.PropsWithChildren<{ href: string }>) => 
-  <a href={href} {...props}>{children}</a>
+  <a href={href} {...props}>{children}</a>;
+
+export const Image = ({ alt = '', ...props }: Omit<ImageProps, 'src'> & { src: string }) =>
+  // eslint-disable-next-line @next/next/no-img-element
+  <img alt={alt} {...props} />;
