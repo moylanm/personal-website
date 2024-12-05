d235556fddfba147a133befbec82687fee55befc
Myles Moylan <moylanm@fastmail.com>
2024-12-05 09:25:47 -0800

    Adjust test configs

--- jest.config.ts
+++ jest.config.ts
@@ -186,7 +186,9 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
