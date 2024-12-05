d52eb784d3c4fc2f8aa31c90cf8d65d797d05d72
Myles Moylan <moylanm@fastmail.com>
2024-12-05 15:47:03 -0800

    Delete `__mocks__` directory

--- __mocks__/custom-mocks.tsx
+++
@@ -1,9 +0,0 @@
-export const NavbarButton = ({ children, href, style }: { 
-  children: React.ReactNode; 
-  href?: string;
-  style?: React.CSSProperties;
-}) => (
-  <button style={style}>
-    <a href={href}>{children}</a>
-  </button>
-)
